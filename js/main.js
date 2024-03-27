2a2445726d664fd7ec7714b61e0bd6258462a21b
8itoDevM <gustavomarcolino862@gmail.com>
2024-03-27 13:30:09 -0300

    Everything updated, cars being added to the database

---
+++ js/main.js
@@ -0,0 +1,56 @@
+var cars = [
+    {
+      name: "Mustang",
+      brand: "Ford",
+      price: 234000,
+      image: "./img/img-mob1.png",
+      quality: "Used"
+    },
+    {
+      name: "Space Odissey",
+      brand: "Marie Anne",
+      price: 35,
+      image: "./assets/products/img1.png",
+      quality: "Used"
+    },
+  ];
+
+
+function slideHandler(){
+    var radio = document.querySelector('.manual-btn');
+    var cont = 1;
+
+    document.getElementById('radio1').checked = true
+
+    setInterval(() => {
+        proximaImg();
+    }, 3000);
+
+    function proximaImg(){
+        cont++;
+
+        if(cont > 3){
+            cont = 1;
+        }
+
+        document.getElementById('radio'+cont).checked = true;
+    }
+
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.querySelector(".nav");
+
+    hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+    function toggleNav() {
+    document.body.classList.toggle("nav-open");
+}
+}
+
+// function footerHandler(){
+//     let currentYear = new Date().getFullYear();
+//     document.querySelector("footer").textContent = `${currentYear} - All rights reserved`
+// }
+
+// handlers
+slideHandler();
+//footerHandler();
