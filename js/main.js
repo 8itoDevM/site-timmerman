370aa7eecd0279512d523c9b128e5791abc607b6
8itoDevM <gustavomarcolino862@gmail.com>
2024-04-01 00:18:28 -0300

    Started to add a responsive main page. The more cars we have on the array more cars appears on the front page, it is almost done so i'm probably gonna finish this tomorrow

--- js/main.js
+++ js/main.js
@@ -3,55 +3,129 @@ var cars = [
       name: "Mustang",
       brand: "Ford",
       price: 234000,
-      image: "./img/img-mob1.png",
-      quality: "Used"
+      image: "./img/imagecarFerrari.png",
+      quality: 0,
+      status: "Destaque da Semana"
     },
     {
-      name: "Space Odissey",
+      name: "teste",
       brand: "Marie Anne",
       price: 35,
-      image: "./assets/products/img1.png",
-      quality: "Used"
+      image: "./img/imagecarFerrari.png",
+      quality: 0,
+      status:
+    },
+    {
+        name: "teste",
+        brand: "Marie Anne",
+        price: 35,
+        image: "./img/imagecarFerrari.png",
+        quality: 0,
+        status:
     },
   ];
 
 
 function slideHandler(){
-    var radio = document.querySelector('.manual-btn');
-    var cont = 1;
+    var radio = document.querySelector('.manual-btn')
+    var cont = 1
 
     document.getElementById('radio1').checked = true
 
     setInterval(() => {
-        proximaImg();
-    }, 3000);
+        proximaImg()
+    }, 3000)
 
     function proximaImg(){
-        cont++;
+        cont++
 
         if(cont > 3){
-            cont = 1;
+            cont = 1 
         }
 
-        document.getElementById('radio'+cont).checked = true;
+        document.getElementById('radio'+cont).checked = true
     }
-
-    const hamburger = document.querySelector(".hamburger");
-    const nav = document.querySelector(".nav");
-
-    hamburger.addEventListener("click", () => nav.classList.toggle("active"));
-
-    function toggleNav() {
-    document.body.classList.toggle("nav-open");
 }
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img\\menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img\\close_white_36dp.svg";
+    }
 }
 
 function populateProducts(productsList){
-    //let productSection = document.querySelector(".products-area");
-    //productSection.textContent = "";
+    let productSection = document.querySelector(".products-area");
+    productSection.textContent = "";
+
     // run a loop through the products and create an HTML element ("product-item") for each of them
-    productsList.forEach(function(cars, index){
-        console.log(cars.name);
+    productsList.forEach(function(car, index){
+        console.log(car.name);
+
+        let productElm = document.createElement("div");
+        productElm.classList.add("product-item");
+
+        let productImage = document.createElement("img");
+        productImage.src = car.image;
+        productImage.alt = "Image for " + car.title;
+
+        let productDetails = document.createElement("div");
+        productDetails.classList.add("product-details");
+
+        let productStatus = document.createElement("div");
+        productStatus.classList.add("status-car");
+
+        let productStatusEvents = document.createElement("h5");
+        productStatusEvents.classList.add("status-events");
+        productStatusEvents.textContent = car.status;
+
+        let productNameAndBrand = document.createElement("div");
+        productNameAndBrand.classList.add("car-title");
+
+        let productName = document.createElement("h3");
+        productName.classList.add("product-brand")
+        productName.textContent = car.name;
+        let productTitle = document.createElement("p");
+        productTitle.classList.add("product-title");
+        productTitle.textContent = car.brand;
+
+        let valorItem = document.createElement("div");
+        valorItem.classList.add("valor-item");
+
+        let typeMoeda = document.createElement("div");
+        typeMoeda.classList.add("type-moeda");
+        typeMoeda.textContent = "R$";
+
+        let valorCar = document.createElement("p");
+        valorCar.classList.add("valor-car");
+        valorCar.textContent = car.price > 0 ? "$"+car.price.toFixed(2) : "Free";
+
+        // Append the product details
+        productDetails.append(productStatus);
+        productDetails.append(productNameAndBrand);
+        productDetails.append(valorItem);
+
+        // Append car price and currency
+        valorItem.append(typeMoeda);
+        valorItem.append(valorCar);
+
+        // Append product status event
+        productStatus.append(productStatusEvents);
+
+        // Append product name and brand
+        productNameAndBrand.append(productName);
+        productNameAndBrand.append(productTitle);
+
+        // add all child HTML element of the product
+        productElm.append(productImage);
+        productElm.append(productDetails);
+
+        // Add complete individual produt to the produt section
+        productSection.append(productElm);
     });
 }
 
@@ -63,6 +137,7 @@ function productsHandler(){
 //     let currentYear = new Date().getFullYear();
 //     document.querySelector("footer").textContent = `${currentYear} - All rights reserved`
 // }
+
 
 // handlers
 slideHandler();
